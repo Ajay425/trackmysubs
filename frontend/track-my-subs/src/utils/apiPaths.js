d17f15a77cb78b07272e1522ede26a53e69a1375
Ajay425 <63657644+Ajay425@users.noreply.github.com>
2025-07-05 12:29:42 -0400

    feat: define centralized API paths and base URL

---
+++ frontend/track-my-subs/src/utils/apiPaths.js
@@ -0,0 +1,25 @@
+export const BASE_URL = "http://localhost:8000";
+
+export const API_PATHS = {
+  AUTH: {
+    LOGIN: "/api/v1/auth/login",
+    REGISTER: "/api/v1/auth/register",
+    GET_USER: "/api/v1/auth/getUser",
+  },
+
+  DASHBOARD: {
+    GET_STATS: "/api/v1/dashboard/stats",
+  },
+
+  SUBSCRIPTIONS: {
+    GET_ALL: "/api/v1/subscriptions",                   // GET
+    GET_BY_ID: (id) => `/api/v1/subscriptions/${id}`,   // GET
+    CREATE: "/api/v1/subscriptions",                    // POST
+    UPDATE: (id) => `/api/v1/subscriptions/${id}`,      // PUT
+    DELETE: (id) => `/api/v1/subscriptions/${id}`,      // DELETE
+  },
+
+  IMAGE: {
+    UPLOAD: "/api/v1/image/upload-image",
+  },
+};
