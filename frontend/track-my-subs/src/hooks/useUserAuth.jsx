47eb3468d41bd361f5b283c2d4c409e120625bd9
Ajay425 <63657644+Ajay425@users.noreply.github.com>
2025-07-08 18:31:58 -0400

    feat; add user authentication hook

---
+++ frontend/track-my-subs/src/hooks/useUserAuth.jsx
@@ -0,0 +1,42 @@
+import { useContext, useEffect } from "react";
+import { UserContext } from "../context/userContext";
+import axiosInstance from "../utils/axiosInstance"; 
+import { API_PATHS } from "../utils/apiPaths";
+import { useNavigate } from "react-router-dom";
+
+
+export const useUserAuth = () => {
+    const  {user, updateUser, clearUser} = useContext(UserContext);
+    const navigate = useNavigate();
+
+    useEffect(() => {
+        if (user) {
+            // User is already authenticated, no need to fetch
+            return;
+        }
+        
+        let isMounted = true; // Track if component is mounted
+
+        const fetchUserInfo = async () => {
+            try {
+                const response = await axiosInstance.get(API_PATHS.AUTH.GET_USER);
+                if (isMounted && response.data) {
+                    updateUser(response.data.user); // Update user context with fetched user data
+                }
+            } catch (error) {
+                console.error("Error fetching user data:", error);
+                if (isMounted) {
+                    clearUser(); // Clear user context on error
+                    navigate("/Login"); // Redirect to login page
+                }
+            }
+        }
+
+                fetchUserInfo();
+        
+                return () => {
+                    isMounted = false;
+                };
+            }, [user, updateUser, clearUser, navigate]);
+        
+        };
