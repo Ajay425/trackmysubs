91ca29af68ed9375f6f00457b9d7cdfa13336b5f
Ajay425 <63657644+Ajay425@users.noreply.github.com>
2025-07-08 18:28:30 -0400

    refactor: add faq page link to navbar

--- frontend/track-my-subs/src/components/Sidebar.jsx
+++ frontend/track-my-subs/src/components/Sidebar.jsx
@@ -1,10 +1,17 @@
 import React from "react";
-import { FiX, FiHome, FiPlus, FiSettings } from "react-icons/fi";
-import { Link } from "react-router-dom";
+import { FiX, FiHome, FiPlus, FiSettings, FiLogOut, FiHelpCircle  } from "react-icons/fi";
+import { Link, useNavigate } from "react-router-dom";
 
 const Sidebar = ({ onClose }) => {
+  const navigate = useNavigate();
+
+  const handleLogout = () => {
+    localStorage.removeItem("token");
+    navigate("/login");
+  };
+
   return (
-    <aside className="w-64 bg-[#101010] border-r border-white/10 p-6 flex flex-col">
+    <aside className="w-64 bg-[#101010] border-r border-white/10 p-6 flex flex-col h-screen">
       {/* Header */}
       <div className="flex justify-between items-center mb-8">
         <h2 className="text-2xl font-bold text-[#7f5af0]">TrackMySubs</h2>
@@ -15,21 +22,38 @@ const Sidebar = ({ onClose }) => {
         )}
       </div>
 
-      {/* Nav */}
-      <nav className="flex flex-col space-y-4">
-        <Link to="/" className="flex items-center gap-2 text-gray-300 hover:text-white">
-          <FiHome />
-          Dashboard
-        </Link>
-        <Link to="/add-subscription" className="flex items-center gap-2 text-gray-300 hover:text-white">
-          <FiPlus />
-          Add Subscription
-        </Link>
-        <Link to="/settings" className="flex items-center gap-2 text-gray-300 hover:text-white">
-          <FiSettings />
-          Settings
-        </Link>
-      </nav>
+      {/* Nav Links and Logout */}
+      <div className="flex flex-col justify-between flex-1">
+        <nav className="flex flex-col space-y-4">
+          <Link to="/" className="flex items-center gap-2 text-gray-300 hover:text-white">
+            <FiHome />
+            Dashboard
+          </Link>
+          <Link to="/add-subscription" className="flex items-center gap-2 text-gray-300 hover:text-white">
+            <FiPlus />
+            Add Subscription
+          </Link>
+          <Link to="/settings" className="flex items-center gap-2 text-gray-300 hover:text-white">
+            <FiSettings />
+            Settings
+          </Link>
+          <Link to="/faq" className="flex items-center gap-2 text-gray-300 hover:text-white">
+          <FiHelpCircle />
+              FAQ
+          </Link>
+        </nav>
+
+        {/* Logout Button */}
+        <div className="pt-8">
+          <button
+            className="flex items-center gap-2 text-gray-300 hover:text-red-500 w-full px-4 py-2 rounded transition justify-center"
+            onClick={handleLogout}
+          >
+            <FiLogOut />
+            Logout
+          </button>
+        </div>
+      </div>
     </aside>
   );
 };
