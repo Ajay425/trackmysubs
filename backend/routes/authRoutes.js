7cd3d4e39cefdbe7e79d9eacb8a2b036a9c16ace
Ajay425 <63657644+Ajay425@users.noreply.github.com>
2025-07-17 18:40:41 -0400

    Update authRoutes.js

--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const { protect } = require("../middlewares/authMiddlewares");
-const upload = require("../middlewares/uploadMiddlewares");
+const { upload } = require("../config/s3");
 
 const {
   registerUser,
@@ -14,15 +14,13 @@ router.post("/register", registerUser);
 router.post("/login", loginUser);
 router.get("/getUser", protect, getUserInfo);
 
-// ✅ Paste it here — below the auth routes
 router.post("/upload-image", upload.single("image"), (req, res) => {
-
   if (!req.file) {
     return res.status(400).json({ message: "No file uploaded" });
   }
 
-  const imageUrl = `${req.protocol}://${req.get("host")}/uploads/${req.file.filename}`;
-  res.status(200).json({ imageUrl });
+  const imageKey = req.file.key; 
+  res.status(200).json({ key: imageKey }); 
 });
 
 module.exports = router;
