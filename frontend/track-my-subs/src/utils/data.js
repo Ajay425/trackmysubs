e348bb07839ac5f6ad499f3ef049043ee84d8ac6
Ajay425 <63657644+Ajay425@users.noreply.github.com>
2025-07-17 11:28:23 -0400

    fix: remove add subscription from side bar

--- frontend/track-my-subs/src/utils/data.js
+++ frontend/track-my-subs/src/utils/data.js
@@ -1,7 +1,6 @@
 // src/data/sidebarData.js
 import {
   FiHome,
-  FiPlus,
   FiSettings,
   FiLogOut,
   FiHelpCircle,
@@ -11,23 +10,17 @@ export const SIDE_MENU_DATA = [
   {
     id: "01",
     label: "Dashboard",
-    path: "/",
+    path: "/home",
     icon: FiHome,
   },
   {
     id: "02",
-    label: "Add Subscription",
-    path: "/add-subscription",
-    icon: FiPlus,
-  },
-  {
-    id: "03",
     label: "Settings",
     path: "/settings",
     icon: FiSettings,
   },
   {
-    id: "04",
+    id: "03",
     label: "FAQ",
     path: "/faq",
     icon: FiHelpCircle,
